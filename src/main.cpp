b5c10b66e6a78e8d1ba4443567e5e55f2ce76947
Timothy James Concepcion <timothyjtc@outlook.com>
2025-01-27 04:36:28 +0800

    ble client test

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,119 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <BLEDevice.h>
 
 // Pin definitions
 const uint8_t LED = 2;                                  // Onboard LED, don't connect to anything
 const uint8_t MPU_SCL = 22;                             // accelerometer SCL
 const uint8_t MPU_SDA = 21;                             // accelerometer SDA
 
+// BLE settings
+static String BLEServerName = "GestureBot";
+static BLEUUID SERVICE_UUID("a16587d4-584a-4668-b279-6ccb940cdfd0");
+static BLEUUID TELEMETRY_CHARUUID("a16587d4-584a-4668-b279-6ccb940cdfd1");
+static BLEUUID CMDSTR_CHARUUID("a16587d4-584a-4668-b279-6ccb940cdfd2");
+static BLEUUID CTLVAL1_CHARUUID("a16587d4-584a-4668-b279-6ccb940cdfd3");
+static BLEUUID CTLVAL2_CHARUUID("a16587d4-584a-4668-b279-6ccb940cdfd4");
+static BLEAddress *pServerAddress;
+
+static boolean doConnect = false;
+static boolean connected = false;
+// static boolean doScan = false;
+
+// Define pointer for the BLE connection
+static BLEAdvertisedDevice* BLEDeviceRobot;
+BLERemoteCharacteristic* TelemetryChar;
+BLERemoteCharacteristic* CmdStrChar;
+BLERemoteCharacteristic* CtlVal1Char;
+BLERemoteCharacteristic* CtlVal2Char;
+
+// Variables
+String telemetry_idle = "";
+String telemetry_drive = "";
+
 // Vehicle controller setup
 const uint8_t MODE_IDLE = 1;
 const uint8_t MODE_DRIVE = 2;
 uint8_t mode = MODE_IDLE;
-
-int threshold = 2.6;
+double threshold = 2.6;
 
 Adafruit_MPU6050 MPU;
+
+//When the BLE Server sends a new temperature reading with the notify property
+static void telemetryNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+  //store temperature value
+  temperatureChar = (char*)pData;
+  newTemperature = true;
+}
+
+// Callback function that is called whenever a client is connected or disconnected
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {
+  }
+
+  void onDisconnect(BLEClient* pclient) {
+    connected = false;
+    Serial.println("onDisconnect");
+  }
+};
+
+//Connect to the BLE Server that has the name, Service, and Characteristics
+bool connectToServer(BLEAddress pAddress) {
+  BLEClient* pClient = BLEDevice::createClient();
+ 
+  // Connect to the remove BLE Server.
+  pClient->connect(pAddress);
+  Serial.println(" - Connected to server");
+ 
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(SERVICE_UUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(SERVICE_UUID.toString().c_str());
+    return (false);
+  }
+ 
+  // Obtain a reference to the characteristics in the service of the remote BLE server.
+  TelemetryChar = pRemoteService->getCharacteristic(TELEMETRY_CHARUUID);
+  CmdStrChar = pRemoteService->getCharacteristic(CMDSTR_CHARUUID);
+
+  if (TelemetryChar == nullptr) {
+    Serial.print("Failed to find our characteristic UUID");
+    return false;
+  }
+  Serial.println(" - Found our characteristics");
+ 
+  //Assign callback functions for the Characteristics
+  TelemetryChar->registerForNotify(telemetryNotifyCallback);
+  return true;
+}
+
+//Callback function that gets called, when another device's advertisement has been received
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    if (advertisedDevice.getName() == BLEServerName) { //Check if the name of the advertiser matches
+      advertisedDevice.getScan()->stop(); //Scan can be stopped, we found what we are looking for
+      pServerAddress = new BLEAddress(advertisedDevice.getAddress()); //Address of advertiser is the one we need
+      doConnect = true; //Set indicator, stating that we are ready to connect
+      Serial.println("Device found. Connecting!");
+    }
+  }
+};
 
 void setup(void) {
   Serial.begin(115200);
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  BLEDevice::init("");
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(30);
 
   // Try to initialize!
   if (!MPU.begin()) {
@@ -96,6 +190,32 @@ void setup(void) {
 }
 
 void loop() {
+
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer(*pServerAddress)) {
+      Serial.println("We are now connected to the BLE Server.");
+      //Activate the Notify property of each Characteristic
+      temperatureCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+      humidityCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+      connected = true;
+    } else {
+      Serial.println("We have failed to connect to the server; Restart your device to scan for nearby BLE server again.");
+    }
+    doConnect = false;
+  }
+  //if new temperature readings are available, print in the OLED
+  if (newTemperature && newHumidity){
+    newTemperature = false;
+    newHumidity = false;
+    printReadings();
+  }
+  delay(1000); // Delay a second between loops.
+
+
+
   sensors_event_t a, g, temp;
   MPU.getEvent(&a, &g, &temp);
 
@@ -127,3 +247,4 @@ void loop() {
   Serial.println("");
   delay(100);
 }
+
